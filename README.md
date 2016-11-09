6f8355e9ce534b2c0ed002c35fb9d57b9ebde453
Attila Beregszaszi <attila@frontseed.com>
2016-11-09 22:57:45 +0000

    Initial import

---
+++ README.md
@@ -0,0 +1,3 @@
+# koa-webpack-dev-hot-middleware
+
+Webpack dev and hot middleware for Koa 1.x
---
+++ src/index.js
@@ -0,0 +1,50 @@
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+
+function middleware (action, request, response) {
+  const end = response.end
+
+  return (done) => {
+    response.end = (...args) => {
+      end.apply(this, args)
+      done()
+    }
+    action(request, response, () => {
+      done(null, true)
+    })
+  }
+}
+
+const webpackDev = (compiler, options) => {
+  const action = webpackDevMiddleware(compiler, options)
+
+  return function* dev (next) {
+    const step = yield middleware(action, this.req, {
+      end: (content) => {
+        this.body = content
+      },
+      setHeader: (...args) => {
+        this.set.apply(this, args)
+      }
+    })
+    if (step) {
+      yield * next
+    }
+  }
+}
+
+const webpackHot = (compiler, options) => {
+  const action = webpackHotMiddleware(compiler, options)
+
+  return function* hot (next) {
+    const step = yield middleware(action, this.req, this.res)
+    if (step) {
+      yield * next
+    }
+  }
+}
+
+module.exports = {
+  webpackDev,
+  webpackHot
+}
